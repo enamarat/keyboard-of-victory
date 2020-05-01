029c6c09cf3b3ea1a59f6795913553e0b3122952
Marat Enaliev <enamarat@yandex.ru>
2020-05-01 17:34:05 +0300

    Prevent the same word appearing on the screen more than once if it is already displayed.

--- script.js
+++ script.js
@@ -8,36 +8,38 @@ const words = [
 
 
 let displayedWords = [];
-console.log(document.querySelector("#arena").offsetWidth);
 const arenaWidth = document.querySelector("#arena").offsetWidth;
 
-//let totalWidth = 0;
 const generateWord = () => {
   let totalWidth = 0;
     const myFunction = function() {
       /**********/
       const word = document.createElement("div");
       word.className = "word";
-      const randomIndex = Math.floor(Math.random() * words.length);
-      const randomWord = words[randomIndex];
+      const randomIndex = () => {
+        return Math.floor(Math.random() * words.length);
+      }
+      let randomWord = words[randomIndex()];
+
+      /* if the same word has already been displayed on the screen and yet not destroyed
+      prevent it from duplicating. Reroll random index and choose another random word */
+      const isEqual = (element) => element.name === randomWord;
+      while (displayedWords.some(isEqual)) {
+        randomWord = words[randomIndex()];
+      }
+
       for (let i = 0; i < randomWord.length; i++) {
         word.innerHTML  +=  `<span>${randomWord[i]}</span>`;
       }
       document.querySelector("#arena").appendChild(word);
       const generatedWords = document.querySelectorAll(".word");
-  const screenWidth = window.screen.width;
+
       displayedWords.push({name:randomWord, count:0});
       generatedWords[generatedWords.length-1].style.left = totalWidth + "px";
       /************/
-
-
-
+      totalWidth += generatedWords[generatedWords.length-1].offsetWidth;
       if (arenaWidth-totalWidth < generatedWords[generatedWords.length-1].offsetWidth) {
-  console.log(`element's width: ${generatedWords[generatedWords.length-1].offsetWidth}`);
-  console.log(`remaining width: ${arenaWidth-totalWidth}`);
         totalWidth = 0;
-      } else {
-        totalWidth += generatedWords[generatedWords.length-1].offsetWidth;
       }
       /*************/
       //generatedWords[generatedWords.length-1].addEventListener("animationend", () => console.log("Game over!"), false);
@@ -52,11 +54,11 @@ const generateWord = () => {
 const foo = generateWord();
 const timer = window.setInterval(()=> {
   foo();
-}, 1000);
+}, 2000);
 
 setTimeout(() => {
   window.clearInterval(timer);
-}, 20000);
+}, 30000);
 
 
 console.log(displayedWords);
@@ -75,6 +77,7 @@ const typeLetters = (event) => {
              document.querySelectorAll(".word")[a].childNodes[b].style.fontWeight = "normal";
            }
         }
+        // remove word if all its letters are type correctly
         if (displayedWords[a].count === displayedWords[a].name.length) {
           document.querySelectorAll(".word")[a].style.backgroundColor = "black";
           setTimeout(() => {
